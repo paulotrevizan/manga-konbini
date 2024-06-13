bbd10d65c50431a4b3c11789e8fc3a47b6c3d1ba
Paulo Trevizan <paulotrevizan@gmail.com>
2024-06-13 12:55:09 +0100

    [MGK-3] Initial project configuration (#1)

---
+++ src/test/java/pt/trevizan/mangakonbini/MangakonbiniApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.trevizan.mangakonbini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangakonbiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
