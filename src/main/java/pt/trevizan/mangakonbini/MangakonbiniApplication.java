5059f186d3ca8a476e047a58aa248da50b6e34ee
Paulo Trevizan <paulotrevizan@gmail.com>
2024-06-13 12:53:19 +0100

    [MGK-3] Initial project configuration

---
+++ src/main/java/pt/trevizan/mangakonbini/MangakonbiniApplication.java
@@ -0,0 +1,13 @@
+package pt.trevizan.mangakonbini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangakonbiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangakonbiniApplication.class, args);
+	}
+
+}
