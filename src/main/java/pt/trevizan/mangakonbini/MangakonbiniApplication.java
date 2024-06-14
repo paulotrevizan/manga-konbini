9bb598cab8f8b59a0fb563ec595f5ed86dcec9d8
Paulo Trevizan <paulotrevizan@gmail.com>
2024-06-14 12:18:44 +0100

    [MGK-31] Testing PALANTIR code format

--- src/main/java/pt/trevizan/mangakonbini/MangakonbiniApplication.java
+++ src/main/java/pt/trevizan/mangakonbini/MangakonbiniApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MangakonbiniApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MangakonbiniApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MangakonbiniApplication.class, args);
+    }
 }
